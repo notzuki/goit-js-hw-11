40d339a67d2236f9aaaec68aeb4929bd0d358319
notzuki <101867510+notzuki@users.noreply.github.com>
2025-08-22 21:01:01 +0300

    request done

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,77 +1,53 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
 
-
-const galleryElem = document.querySelector('.gallery');
-let lightbox = new SimpleLightbox(".gallery a");
-
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 export function createGallery(images) {
-    const markup = images
-        .map((image) => 
-            `<li class="gallery-item">
-            <a class="gallery-link" href="${image.largeImageURL}">
-            <img
-            class="gallery-image"
-            src="${image.webformatURL}"
-            data-source="${image.largeImageURL}"
-            alt="${image.tags}"
-            />
-            <div class = "gallery-box">
-            <p class="gallery-box-text">Likes <span class="gallery-box-span">${image.likes}</span></p>
-            <p class="gallery-box-text">Views <span class="gallery-box-span">${image.views}</span></p>
-            <p class="gallery-box-text">Comments <span class="gallery-box-span">${image.comments}</span></p>
-            <p class="gallery-box-text">Downloads <span class="gallery-box-span">${image.downloads}</span></p>
-            </div>
-            </a>
-            </li>`
-        )
-        .join('');
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li>
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="info">
+          <p>Likes: ${likes}</p>
+          <p>Views: ${views}</p>
+          <p>Comments: ${comments}</p>
+          <p>Downloads: ${downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join("");
 
-    galleryElem.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh();
-}
+    galleryEl.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
+};
 
 export function clearGallery() {
-    galleryElem.innerHTML = '';
-}
+  galleryEl.innerHTML = "";
+};
 
 export function showLoader() {
-    document.querySelector('.loader').classList.remove('is-hidden');
-}
+  loaderEl.classList.remove("hidden");
+};
 
 export function hideLoader() {
-    document.querySelector('.loader').classList.add('is-hidden');
-}
-
-export function showLoadMoreButton() {
-    document.querySelector('.btn-load').classList.remove('is-hidden');
-}
-
-export function hideLoadMoreButton() {
-    document.querySelector('.btn-load').classList.add('is-hidden');
-}
-
-export function loadMoreBtnVisibleStatus(totalHits, page, per_page = 15) {
-    const maxPage = Math.ceil(totalHits / per_page);
-    if(page < maxPage) {
-            showLoadMoreButton();
-        } else {
-            hideLoadMoreButton();
-            iziToast.info({
-                message: "We're sorry, but you've reached the end of search results.",
-            });
-        };
-}
-
-export function scrollNewContent() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
+  loaderEl.classList.add("hidden");
+};
